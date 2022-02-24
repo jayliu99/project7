3e5aa85dee2b9d0e98a267ec0a28b547914fa8c8
Jay Liu <jminsuliu@gmail.com>
2022-02-24 09:29:38 -0800

    Wrote requirements.txt, set up pip installable module

---
+++ nn/__init__.py
@@ -0,0 +1,11 @@
+
+"""
+BMI203: Biocomputing algorithms Winter 2022
+Assignment 7: Final Project: Neural Network
+"""
+
+from .io import (read_text_file, read_fasta_file)
+from .nn import NeuralNetwork
+from .preprocess import (one_hot_encode_seqs, sample_seqs)
+
+__version__ = '0.1.0'
